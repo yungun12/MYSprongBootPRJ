f4d4b026c0e7d8c2d82207948391bf4ce311cd60
윤건 <yungun200512@gmail.com>
2024-12-11 17:50:17 +0900

    Email DTO 추가

---
+++ src/main/java/kopo/poly/dto/MailDTO.java
@@ -0,0 +1,16 @@
+package kopo.poly.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@ToString
+public class MailDTO {
+
+    String toMail; // 받는 사람
+    String title; // 보내는 메일 제목
+    String contents; // 보내는 메일 내용
+
+}
