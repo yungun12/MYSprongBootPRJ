58dfce3411337ac5be1801a5d8b5e14e5df56876
윤건 <yungun200512@gamil.com>
2024-12-26 23:59:42 +0900

    회원가입 DTO 제작

---
+++ src/main/java/kopo/poly/dto/UserInfoDTO.java
@@ -0,0 +1,31 @@
+package kopo.poly.dto;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@JsonInclude(JsonInclude.Include.NON_DEFAULT)
+public class UserInfoDTO {
+
+    private String userId;
+    private String userName;
+    private String password;
+    private String email;
+    private String addr1;
+    private String addr2;
+    private String regId;
+    private String regDt;
+    private String chgId;
+    private String chgDt;
+
+    // 회원 가입시 중복가입을 방지하기 위해 사용할 변수
+    // DB를 조회해서 회원이 존재하면 Y값을 반환함
+    // DB테이블에 존재하지 않는 가상의 컬럼
+    private String existsYn;
+
+    // 이메일 중복체크를 위한 인증번호
+    private int authNumber;
+
+}
